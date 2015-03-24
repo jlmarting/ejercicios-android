9d8d4d94356967545bf5f055a3f0f4c907d75b6f
JL <jlmartingalvan@gmail.com>
2015-03-24 19:01:08 +0100

    Problema de workspace con Intent solucionado

--- Intents/app/src/main/java/com/jlmarting/intents/MainActivity.java
+++ Intents/app/src/main/java/com/jlmarting/intents/MainActivity.java
@@ -1,116 +1,19 @@
 package com.jlmarting.intents;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    /*
-    TODOS:
-    crear un método addEventListeners() para centralizar desde ahí la gestión de Listeners
-
-    getApplicationContext <---> MainActivity.this
-
-    para el uso de la cámara debería introducirse declaraciones en el manifiesto
-
-     */
-
-    private static final int REQUEST_TARGET = 1;
-    public static final String MSG = "MSG";
-    private static final int REQUEST_PHOTO = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnSend = (Button)findViewById(R.id.btnSendMsg);
-        Button btnPhoto = (Button)findViewById(R.id.btnPhoto);
-
-
-        btnPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                if(takePictureIntent.resolveActivity(getPackageManager())!=null){
-                    startActivityForResult(takePictureIntent, REQUEST_PHOTO);
-                }
-
-            }
-        });
-
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText etMsg = (EditText)findViewById(R.id.etMsg);
-                String msg = etMsg.getText().toString();
-
-                if(msg.length()>0){
-                    Intent intent = new Intent(getApplicationContext(),MessageActivity.class);
-                    intent.putExtra(MSG, msg);
-
-                    /*
-                    Indicamos un código para poder identificar después la respuesta
-                    es decir, de que actividad nos vienen los datos
-                    */
-                    startActivityForResult(intent, REQUEST_TARGET);
-                }
-                else {
-                    //TOAST: mensajes en pantalla
-                    Toast toast = Toast.makeText(MainActivity.this,
-                            getResources().getString(R.string.no_text),
-                            Toast.LENGTH_LONG);
-                    toast.show();
-
-                }
-
-            }
-        });
-
     }
 
-    /* Este método siempre se ejecuta al finalizar una actividad,
-    analizaremos el código y actuaremos en consecuencua
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode!=RESULT_CANCELED){
-            switch(resultCode){
-                case REQUEST_TARGET:
-                    String msg = data.getStringExtra(MSG);
-                    TextView lblMsg = (TextView)findViewById(R.id.lblMsg);
-                    lblMsg.setText(msg);
-                    break;
-                case REQUEST_PHOTO:
-                    Log.d("REQUEST_PHOTO","Mostrando foto....");
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap)extras.get("data");
-                    ImageView imageView = (ImageView)findViewById(R.id.imageView);
-                    imageView.setImageBitmap(imageBitmap);
-                    break;
-
-            }
-        }
-
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
