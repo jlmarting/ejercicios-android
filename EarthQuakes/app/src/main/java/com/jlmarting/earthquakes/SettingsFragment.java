92e5c3cdf2c84d4c80a6050daa56f35b3a495d49
JL <jlmartingalvan@gmail.com>
2015-03-31 19:29:07 +0200

    EarthQuakes: implementando alarmas, sin terminar

--- EarthQuakes/app/src/main/java/com/jlmarting/earthquakes/SettingsFragment.java
+++ EarthQuakes/app/src/main/java/com/jlmarting/earthquakes/SettingsFragment.java
@@ -1,7 +1,11 @@
 package com.jlmarting.earthquakes;
 
 
+import android.app.AlarmManager;
 import android.app.Fragment;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
@@ -18,6 +22,8 @@ import java.util.Map;
  */
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+
+    private static final int ALARM_EARTHQUAKES = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,7 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         Map<String, ?> settings = (Map) sharedPreferences.getAll();
 
         //No se puede hacer un switch de strings...
+
         String PREF_AUTO_UPDATE = getString(R.string.PREF_AUTO_UPDATE);
         String PREF_UPDATE_INTERVAL = getString(R.string.PREF_UPDATE_INTERVAL);
         //String PREF_MIN_MAG = getString(R.string.PREF_MIN_MAG);
@@ -45,12 +52,19 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         Por lo tanto no nos tenemos que preocupar de pasar valores.
         En la actividad o fragmento (como en este caso) en onResume puede ser buena idea
         para leer estos parámetros
-
          */
 
         if (key.equals(PREF_AUTO_UPDATE)) {
 
         } else if (key.equals(PREF_UPDATE_INTERVAL)) {
+            AlarmManager alarmManager =
+                    (AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+
+            int alarmType = AlarmManager.ELAPSED_REALTIME;
+            String ALARM_ACTION = "ALARM_ACTION";
+            Intent intentToFire = new Intent(ALARM_ACTION);
+            PendingIntent alarmIntent = PendingIntent.getService(getActivity(),ALARM_EARTHQUAKES,)
+            alarmManager.set(alarmType,100,intentToFire);
 
         } else if (key.equals(PREF_AUTO_UPDATE)) {
             double minMag = Double.parseDouble(sharedPreferences.getString(key, "0"));
