5be104e94551fcbba9a602d616cfc24e0938c49c
JL <jlmartingalvan@gmail.com>
2015-04-10 19:29:08 +0200

    EarthQuakes: insertando TABS

--- EarthQuakes/app/src/main/java/com/jlmarting/earthquakes/MainActivity.java
+++ EarthQuakes/app/src/main/java/com/jlmarting/earthquakes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jlmarting.earthquakes;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,11 +11,12 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.jlmarting.earthquakes.database.EarthQuakeDB;
+import com.jlmarting.earthquakes.fragments.EarthQuakeListFragment;
 import com.jlmarting.earthquakes.managers.EarthQuakeAlarmManager;
 import com.jlmarting.earthquakes.tasks.DowloadEarthQuakesTask;
 
 
-public class MainActivity extends ActionBarActivity implements DowloadEarthQuakesTask.AddEarthQuakeInterface {
+public class MainActivity extends Activity implements DowloadEarthQuakesTask.AddEarthQuakeInterface {
 
     private static final int PREFS_ACTIVITY = 1 ;
     private EarthQuakeDB dbHelper;
@@ -25,7 +27,6 @@ public class MainActivity extends ActionBarActivity implements DowloadEarthQuake
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         String KEY = "LAUNCHED_BEFORE";
         SharedPreferences prefs= getSharedPreferences(EARTHQUAKE_PREFS, Activity.MODE_PRIVATE);
 
@@ -34,7 +35,30 @@ public class MainActivity extends ActionBarActivity implements DowloadEarthQuake
             EarthQuakeAlarmManager.setAlarm(this,interval);
             prefs.edit().putBoolean(KEY, true).apply();
         }
+
+
+        ActionBar actionBar = this.getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+
+        /*
+            Sin el setTabListener no funciona:
+            ¿por qué? ¿Es obligatorio asignar un listener?
+        */
+        ActionBar.Tab tab1 = actionBar.newTab().setText("TEST1")
+                .setTabListener(new TabListener<EarthQuakeListFragment>());
+
+        ActionBar.Tab tab2 = actionBar.newTab().setText("TEST2")
+                .setTabListener(new TabListener<EarthQuakeListFragment>());
+
+
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
+
+
     }
+
 
 
     @Override
